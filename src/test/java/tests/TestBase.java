fc05ca7f8bba686dc208ac2bf8ee05206e66f178
AlexeyM <alex_mesch@mail.ru>
2022-09-18 01:47:27 +0300

    added owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,7 +12,7 @@ import pages.RegistrationFormPage;
 import pages.components.Attach;
 
 public class TestBase {
-
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     @BeforeAll
@@ -22,10 +24,14 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = config.baseUrl();
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("version");
+        Configuration.browserSize = System.getProperty("browserSize");
+        String remoteDriverUrl = System.getProperty("remoteDriverUrl");
+
+        Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), remoteDriverUrl);
     }
 
     @AfterEach
